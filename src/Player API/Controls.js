49311a58e0eded42a8e848adc74499f2ee5e2bf3
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-26 00:33:22 -0700

    set axios headers and plugged in onClick functions.

--- src/Player API/Controls.js
+++ src/Player API/Controls.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-const Controls = () => {
+axios.defaults.headers.common["Authorization"] =
+  "Token 9cc02eba9fa61e9154c47ebfe0bb99591d17faa9";
+
+const Controls = (props) => {
+  
   return (
     <div className='controller'>
-      <button className='directions'>◄</button>
-      <button className='directions'>▲</button>
-      <button className='directions'>▼</button>
-      <button className='directions'>►</button>
+      <button className='directions' onClick={e => props.handleClick(e)} value='w'>
+        ◄
+      </button>
+      <button className='directions' onClick={e => props.handleClick(e)} value='n'>
+        ▲
+      </button>
+      <button className='directions' onClick={e => props.handleClick(e)} value='s'>
+        ▼
+      </button>
+      <button className='directions' onClick={e => props.handleClick(e)} value='e'>
+        ►
+      </button>
     </div>
   );
 };
