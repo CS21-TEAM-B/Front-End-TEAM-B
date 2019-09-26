9d62188bf93f30dfe9e9279c1e8a5f7d86272404
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-26 01:04:14 -0700

    using lambda testing server until backend api is working.

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import './Auth.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Auth.css";
 
 function Register(props) {
-
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
-
-
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -20,53 +17,74 @@ function Register(props) {
       password2: passwordConfirm
     };
 
-    axios.post(`https://mudapp.herokuapp.com/api/registration/`, {...user})
+    axios
+      .post(`https://lambda-mud-test.herokuapp.com/api/registration/`, {
+        ...user
+      })
       .then(res => {
         console.log(res);
         console.log(res.data);
-        props.history.push('/gamemap')
-      })
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Token ${res.data.key}`;
+        props.history.push("/gamemap");
+      });
+  };
 
-  }
-
-  console.log(userName, password, passwordConfirm)
+  console.log(userName, password, passwordConfirm);
 
   return (
-    <div className="register">
-        <form className="register_form" onSubmit={handleSubmit}>
-            <h2>Welcome</h2>
-            <div className="input">
-              <input className="input-content" 
-              type="text" 
-              name="username" required 
-              placeholder="Username" 
-              value={userName} 
-              onChange={(e) => setUserName(e.target.value)}/>
-            </div>
+    <div className='register'>
+      <form className='register_form' onSubmit={handleSubmit}>
+        <h2>Welcome</h2>
+        <div className='input'>
+          <input
+            className='input-content'
+            type='text'
+            name='username'
+            required
+            placeholder='Username'
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
+        </div>
 
-            <div className="input">
-              <input className="input-content" 
-              type="password" 
-              name="password" required 
-              placeholder="Password" 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}/>
-            </div>
+        <div className='input'>
+          <input
+            className='input-content'
+            type='password'
+            name='password'
+            required
+            placeholder='Password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
 
-            <div className="input">
-              <input className="input-content" 
-              type="password" 
-              name="password" required 
-              placeholder="Confirm Password" 
-              value={passwordConfirm}
-              onChange={(e) => setPasswordConfirm(e.target.value)}/>
-            </div>
+        <div className='input'>
+          <input
+            className='input-content'
+            type='password'
+            name='password'
+            required
+            placeholder='Confirm Password'
+            value={passwordConfirm}
+            onChange={e => setPasswordConfirm(e.target.value)}
+          />
+        </div>
 
-            <p>Already have an account? <Link to="/login" className="link-wrapper"><span className="redirect_link">Sign in</span></Link></p>
-            <div className="button_wrapper">
-              <button className="register_button" type="submit">Start</button>
-            </div> 
-        </form>
+        <p>
+          Already have an account?{" "}
+          <Link to='/login' className='link-wrapper'>
+            <span className='redirect_link'>Sign in</span>
+          </Link>
+        </p>
+        <div className='button_wrapper'>
+          <button className='register_button' type='submit'>
+            Start
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
