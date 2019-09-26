2dd91acc605b3c78be7169b836d8e3a88d781ef5
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-26 00:32:00 -0700

    added error messages and player list

--- src/Player API/SideMenu.js
+++ src/Player API/SideMenu.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const SideMenu = () => {
-  return <div className='side-menu'>hello</div>;
+const SideMenu = props => {
+  console.log(props);
+  const players = props.details.players;
+
+  console.log(players);
+
+  if (props.details) {
+    return (
+      <div className='side-menu'>
+        <div className='player-stats'>
+          <h3>{props.details.title}</h3>
+          <p>{props.details.description}</p>
+        </div>
+        <div className='players'>
+          <h3>Players Here:</h3>
+          {players
+            ? players.map(player => <p>{player}</p>)
+            : "No players currently here"}
+        </div>
+        <div className='chat'>bottom menu</div>
+      </div>
+    );
+  } else {
+    return <div className='side-menu'>LOADING...</div>;
+  }
 };
 
 export default SideMenu;
