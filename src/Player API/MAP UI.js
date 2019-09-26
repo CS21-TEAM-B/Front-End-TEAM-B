2bde912fc9fbc9c893e2dc4f9566c6f50dee3096
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-25 21:51:49 -0700

    main map body

--- src/Player API/MAP UI.js
+++ src/Player API/MAP UI.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './MAP UI.scss'
-import Controls from './Controls';
+import "./MAP UI.scss";
+import Controls from "./Controls";
+import SideMenu from "./SideMenu";
 
 const Map = () => {
+  const [map, setMap] = useState([]);
+  const [direction, setDirection] = useState("");
 
-    const [map, setMap] = useState([])
-    const [direction, setDirection] = useState('')
-    
-    return <div className="main">
-        <div className="map-container">
-            <div className="map">
-                hello
-            </div>
-            <Controls/>
-            </div>
-        </div> 
-}
- 
+  return (
+    <div className='main'>
+      <div className='map-container'>
+        <div className='map'>hello</div>
+        <SideMenu></SideMenu>
+        <Controls />
+      </div>
+    </div>
+  );
+};
+
 export default Map;
